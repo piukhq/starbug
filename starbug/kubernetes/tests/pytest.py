f1ef146a93618364a60f18a1d8685a3d6d803d18
Navya <navyaaj@gmail.com>
2024-02-08 17:45:08 +0000

    pytest test suite

--- starbug/kubernetes/tests/pytest.py
+++ starbug/kubernetes/tests/pytest.py
@@ -1,43 +1,40 @@
-"""Defines a Pyqa-apiv2 Instance."""
+"""Provides a pytest based test suite."""
 
-from kr8s.objects import Deployment, Job, RoleBinding, Service, ServiceAccount
-# unable to import from kr8s
+from kr8s.objects import Job, Role, RoleBinding, ServiceAccount
+
 from starbug.kubernetes import get_secret_value, wait_for_pod
-from starbug.kubernetes.custom.resources import SecretProviderClass
+from starbug.kubernetes.internal.scutter import scutter_container, scutter_role, scutter_rolebinding
 
-class Pyqa:
-    "Defines a Pyqa Instance."
+class Pytest:
+    """Provides a pytest based test suite."""
 
-    def __init__(self, name, namespace, image, secret, replicas=1):
+    def __init__(self, namespace:str, image:str| None = None) -> None:
+        """Initialize the Pytest Class."""
+        self.name = "pytest"
         self.namespace = namespace
-        self.name = "pyqa-apiv2"
         self.image = image or "binkcore.azurecr.io/pyqa-apiv2:latest"
-        self.labels = {"app": "pyqa-apiv2"}
         self.env = {
-            "ALERT_ON_FAILURE": "True",
-            "ALERT_ON_SUCESS": "True",
-            "COMMAND": "pytest --html report.html --self-contained-html -m bink_regression_api2 --channel bink --env staging",
-            "FRIENDLY_NAME": "Staging - Bank API V2.0",
-            "REPORT_CONTAINER": "qareports",
-            "REPORT_DIRECTORY": "qareports",
-            "SCHEDULE": "0 0 * * *",
-            "TEAMS_WEBHOOK": "https://hellobink.webhook.office.com/webhookb2/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/0856493823a1484b9adfa37c942d2da4/48aca6b1-4d56-4a15-bc92-8aa9d97300df"
+            "BLOB_STORAGE_ACCOUNT_DSN": get_secret_value("azure-storage", "blob_connection_string_primary"),
+            "HERMES_DATABASE_URI":get_secret_value("azure-postgres", "url_hermes"),
+            "HARMONIA_DATABASE_URI": get_secret_value("azure-postgres", "url_harmonia"),
+            "SNOWSTORM_DATABASE_URI": get_secret_value("azure-postgres", "url_snowstorm"),
+            "VAULT_URL": get_secret_value("azure-keyvault", "url"),
         }
-
-
         self.serviceaccount = ServiceAccount(
             {
                 "apiVersion": "v1",
                 "kind": "ServiceAccount",
                 "metadata": {
                     "annotations": {
-                        "azure.workload.identity/client-id": get_secret_value("azure-identities", "pyqa_client_id"),
+                        "azure.workload.identity/client-id": get_secret_value("azure-identities", "pytest_client_id"),
                     },
                     "name": self.name,
                     "namespace": self.namespace,
                 },
             },
         )
+        self.scutter_role = scutter_role(namespace=self.namespace)
+        self.scutter_rolebinding = scutter_rolebinding(namespace=self.namespace, service_account_name=self.name)
         self.rolebinding = RoleBinding(
             {
                 "apiVersion": "rbac.authorization.k8s.io/v1",
@@ -60,19 +57,24 @@ class Pyqa:
                 ],
             },
         )
-        self.migrator = Job(
+        self.test = Job(
             {
                 "apiVersion": "batch/v1",
                 "kind": "Job",
                 "metadata": {
-                    "name": self.name + "-migrator",
+                    "name": self.name,
                     "namespace": self.namespace,
-                    "labels": self.labels,
+                    "labels":{
+                        "app": self.name,
+                    },
                 },
                 "spec": {
                     "template": {
                         "metadata": {
-                            "labels": self.labels,
+                            "name": self.name,
+                            "labels": {
+                                "app": self.name,
+                            },
                             "annotations": {
                                 "kubectl.kubernetes.io/default-container": self.name,
                             },
@@ -90,8 +92,8 @@ class Pyqa:
                                 },
                             ],
                             "serviceAccountName": self.name,
-                            "restartPolicy": "Never",
                             "imagePullSecrets": [{"name": "binkcore.azurecr.io"}],
+                            "initContainers": [wait_for_pod("angelia")],
                             "containers": [
                                 {
                                     "name": self.name,
@@ -99,17 +101,29 @@ class Pyqa:
                                     "env": [{"name": k, "value": v} for k, v in self.env.items()],
                                     "command": ["linkerd-await", "--"],
                                     "args": [
-                                        "bash",
-                                        "-c",
-                                        "pytest --html /tmp/report.html --self-contained-html -m bink_regression_api2 --channel bink --env staging && echo $? > /tmp/status.txt || true"],
+                                        "pytest",
+                                        "--html",
+                                        "/tmp/report.html",
+                                        "--self-contained-html",
+                                        "-m bink_regression_api2 --channel bink --env staging",
+                                    ],
+                                    "volumeMounts": [{"name": "results", "mountPath": "/mnt/results"}],
                                     "securityContext": {
                                         "runAsGroup": 0,
                                         "runAsUser": 0,
                                     },
                                 },
+                                scutter_container(filename="report.html"),
                             ],
+                            "restartPolicy": "Never",
+                            "volumes": [{"name": "results", "emptyDir": {"medium": "Memory"}}],
                         },
                     },
+                    "backoffLimit": 0,
                 },
             },
         )
+
+    def deploy(self) -> tuple[ServiceAccount, Role, RoleBinding, RoleBinding, Job]:
+        """Deploy the Pytest Test Suite."""
+        return (self.serviceaccount, self.scutter_role, self.scutter_rolebinding, self.rolebinding, self.test)
