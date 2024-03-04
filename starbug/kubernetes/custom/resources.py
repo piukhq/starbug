8c2c0dad5cc7f8f67c69ce971552dfb1a9bfdbd3
Chris Pressland <mail@cpressland.io>
2024-03-04 20:45:01 +0000

    Fix crd definition

--- starbug/kubernetes/custom/resources.py
+++ starbug/kubernetes/custom/resources.py
@@ -6,7 +6,7 @@ from kr8s.objects import APIObject
 class StarbugTest(APIObject):
     """StarbugTest Class."""
 
-    version = "bink.com/v1beta1"
+    version = "bink.com/v1"
     endpoint = "tests"
     kind = "StarbugTest"
     plural = "tests"
