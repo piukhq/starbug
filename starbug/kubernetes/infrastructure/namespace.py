099314ef0f7cd6fdaf667cc303ca71f2d3a22e60
Chris Pressland <mail@cpressland.io>
2024-03-04 21:15:35 +0000

    Revert namespace changes introduced in v1.2.0

---
+++ starbug/kubernetes/infrastructure/namespace.py
@@ -0,0 +1,43 @@
+"""Defines a Namespace."""
+
+import json
+
+from kr8s.objects import Namespace
+
+
+class AITNamespace:
+    """Defines a Namespace."""
+
+    def __init__(self, name: str) -> None:
+        """Initialize the Namespace Class."""
+        self.name: str = name
+        self.linkerd_inject: str = "enabled"
+        self.tolerations: list = [
+            {"key": "bink.com/workload", "operator": "Equal", "value": "txm", "effect": "NoSchedule"},
+            {
+                "key": "kubernetes.azure.com/scalesetpriority",
+                "operator": "Equal",
+                "value": "spot",
+                "effect": "NoSchedule",
+            },
+        ]
+        self.node_selector: str = "kubernetes.azure.com/scalesetpriority=spot"
+
+        self.namespace = Namespace(
+            {
+                "apiVersion": "v1",
+                "kind": "Namespace",
+                "metadata": {
+                    "annotations": {
+                        "linkerd.io/inject": self.linkerd_inject,
+                        "scheduler.alpha.kubernetes.io/defaultTolerations": json.dumps(self.tolerations),
+                        "scheduler.alpha.kubernetes.io/node-selector": self.node_selector,
+                    },
+                    "name": self.name,
+                },
+            },
+        )
+
+    def deploy(self) -> tuple[Namespace]:
+        """Return all deployable objects as a tuple."""
+        return (self.namespace,)
